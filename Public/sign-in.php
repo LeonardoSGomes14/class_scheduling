2dac4c931d40bb702a69a8d3c3ad8bf164d3f44e
NECR0XX <carronananiasx@gmail.com>
2024-09-11 15:31:41 -0300

    3º - criação de configuração de cargos de login

---
+++ Public/sign-in.php
@@ -0,0 +1,22 @@
+<?php
+include_once '../Config/config.php';
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign In</title>
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+        <section>
+            
+        </section>
+    </main>
+</body>
+</html>
