25add294d7f773c12959b6c8b4ff080732eb1f58
veniceos <vinicius.canevari1@gmail.br>
2024-09-13 14:59:32 -0300

    commit menu sanduiche

--- Public/index.php
+++ Public/index.php
@@ -6,11 +6,53 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <link rel="stylesheet" href="../Resources/Css/sandwich-menu.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=SUSE:wght@100..800&display=swap" rel="stylesheet">
+    <title>Página Inicial</title>
 </head>
 <body>
     <header>
-
+        <div class="menu-toggle" id="menu-toggle">
+            <svg class="menu-open-button" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 72 72">
+                <path d="M56 48c2.209 0 4 1.791 4 4 0 2.209-1.791 4-4 4-1.202 0-38.798 0-40 0-2.209 0-4-1.791-4-4 0-2.209 1.791-4 4-4C17.202 48 54.798 48 56 48zM56 32c2.209 0 4 1.791 4 4 0 2.209-1.791 4-4 4-1.202 0-38.798 0-40 0-2.209 0-4-1.791-4-4 0-2.209 1.791-4 4-4C17.202 32 54.798 32 56 32zM56 16c2.209 0 4 1.791 4 4 0 2.209-1.791 4-4 4-1.202 0-38.798 0-40 0-2.209 0-4-1.791-4-4 0-2.209 1.791-4 4-4C17.202 16 54.798 16 56 16z"></path>
+            </svg>
+        </div>
+        <nav id="menu" class="menu">
+            <div class="container-up-SM">
+                <div class="close-menu" id="close-menu">
+                    <img class="menu-close-button" src="../Resources/Images/close.png" alt="close-icon">
+                </div>
+                <div class="logout">
+                    <a href="#">
+                        <img class="logout-button" src="../Resources/Images/sign-out.png" alt="logout-icon">
+                    </a>    
+                </div>
+            </div>
+            <div class="list-nav">
+                <ul class="ul-nav">
+                    <li class="button-nav">
+                        <a class="link-nav" href="">
+                            <img class="icons" src="../Resources/Images/calendar.png" alt="Calendario-icon">
+                            <p class="nav-text">CALENDÁRIO</p>
+                        </a>
+                    </li>
+                    <li class="button-nav">
+                        <a class="link-nav" href="">
+                            <img class="icons-arrow" src="../Resources/Images/play_arrow.png" alt="Calendario-icon">
+                            <p class="nav-text">SALAS DISPONÍVEIS</p>
+                        </a>
+                    </li>
+                    <li class="button-nav">
+                        <a class="link-nav" href="">
+                            <img class="icons-arrow" src="../Resources/Images/play_arrow.png" alt="Calendario-icon">
+                            <p class="nav-text">SALAS AGENDADAS</p>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
     </header>
     <main>
         <section>
@@ -19,3 +61,4 @@ session_start();
     </main>
 </body>
 </html>
+<script src="../Resources/Js/sandwich-menu.js"></script>
